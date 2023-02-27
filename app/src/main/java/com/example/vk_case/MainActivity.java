be33a7d7ebf92b227ea734d67214d1e784247e1f
MosSla <mossla@inbox.ru>
2023-02-27 17:46:11 +0300

    added two fragments and actions for call button, rised hand button & chat button (btw, i didn't know how i can open sys sms-app from my own app)

--- app/src/main/java/com/example/vk_case/MainActivity.java
+++ app/src/main/java/com/example/vk_case/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.vk_case;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.vk_case.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // при нажатии на кнопку завершения вызова - выход из приложения
+        binding.callEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        // поднятая рука - вызов диалога
+        binding.riseHand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog();
+            }
+        });
+        // переход на приложение для сообщений
+        binding.chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:777"));
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    // Define what your app should do if no activity can handle the intent.
+                }
+            }
+        });
+    }
+    // отдельная функция, отвечающая за диалог
+    private void myDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.hi);
+        builder.setMessage(R.string.message);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
